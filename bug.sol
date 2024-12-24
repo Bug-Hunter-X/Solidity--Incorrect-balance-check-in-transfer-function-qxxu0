03bf5f4e5cd4de74cec688c04fb317471835b139
BugHunter-X <askzenai@gmail.com>
2024-12-24 21:44:05 +0100

    Add bug.sol file

---
+++ bug.sol
@@ -0,0 +1,14 @@
+```solidity
+function transfer(address to, uint amount) public {
+  require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+  balanceOf[msg.sender] -= amount;
+  balanceOf[to] += amount;
+  emit Transfer(msg.sender, to, amount);
+}
+
+// ... other functions
+
+function balanceOf(address account) public view returns (uint) {
+  return balances[account]; // Incorrect usage of balances
+}
+```
